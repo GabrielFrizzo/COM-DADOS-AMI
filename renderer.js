85ece67d72a2481378695d604065903373b1c8a9
Gabriel Frizzo <gabrielfrizzo@gmail.com>
2019-04-16 18:52:58 -0300

    Add binary plot

--- renderer.js
+++ renderer.js
@@ -1,12 +1,45 @@
 $(() => {
+	function XYdata(binary) {
+		var x = [0]
+		var y = [binary[0]]
+		var count = 0
+		for (const bit of binary.substr(1)){
+			if (bit != y.slice(-1)[0]) {
+				x.push(count)
+				y.push(bit)
+
+			}
+			count += 1
+			x.push(count)
+			y.push(bit)
+		}
+
+		return [{x: x, y: y}]
+	}
+
 	$('#message').bind('input propertychange', function() {
 		const text = this.value
 
-		const binary = text.split('').map(function (char) { return char.charCodeAt(0).toString(2); }).join(' ');
+		const binary = text.split('').map(function (char) { return char.charCodeAt(0).toString(2); }).join('');
 		
-		console.log(binary)
 		$('#binary_message').val(binary)
+
+		const layout = {
+			yaxis: {
+				range: [-1, 1],
+				autorange:false,
+				showgrid: false,
+				nticks: 3
+			},
+			xaxis: {
+				showgrid: false,
+				showline: false
+			}
+			// margin: { t: 0 }
+		}
+		Plotly.newPlot( 'graph', XYdata(binary), layout )
 	})
 
 	$('#message').focus()
+
 })
